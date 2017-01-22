ce287416b06db4d8a83823b2de3b524101938554
Игорьков Николай < >
2017-01-23 01:08:46 +0300

    add const

--- bot.py
+++ bot.py
@@ -1,8 +1,1 @@
-OPERATOR_REGISTRATION = "Hello, Input information of new client"
-OPERATOR_FIO = "Input client's firstname and secondname: "
-OPERATOR_AGE ="Input clinet's age: "
-OPERATOR_SUM ="Input monthly payment client: "
-DOCTOR_START = "Hello, doctor. Input ID of client: "
-DOCTOR_UPDATE = "Enter an updated medical history: "
-DOCTOR_VIEW   =  "Medical history of the client"
 
---
+++ const.py
@@ -0,0 +1,26 @@
+START_MESSAGE = "Hello! Please, Log in!"
+OPERATOR_START = "Hello, operator"
+OPERATOR_CLIENT_REGISTRATION = "Input information of new client"
+OPERATOR_USER_REGISTRATION = "Input information of new system user"
+OPERATOR_REMOVING_PROFILE = "Enter ID profile you want to delete:"
+OPERATOR_FIO = "Input client's firstname and secondname: "
+OPERATOR_AGE =  "Input clinet's age: "
+OPERATOR_SUM =  "Input monthly payment client: "
+DOCTOR_START =  "Hello, doctor. Input ID of client: "
+DOCTOR_UPDATE_PROFILE = "Enter an updated medical history: "
+DOCTOR_VIEW_PROFILE  = "Medical history of the client"
+REGISTRATION_OK = "Success! New client is registered."
+UPDATE_TEXT_OK = "Medical history rewritten!"
+NEW_TEXT_OK = "Client's  history stored"
+
+#operations
+OPERATOR_BUTTON_REG_CLIENT = "New client registration"
+OPERATOR_BUTTON_REG_COURIER = "New courier registration"
+OPERATOR_BUTTON_DELETE = "Remove the profile from the system"
+DOCTOR_BUTTON_SHOW_TEXT = "Show medical history of client"
+DOCTOR_BUTTON_NEW_TEXT = "Update medical history"
+DOCTOR_BUTTON_DELETE = "Delete medical history"
+
+#error
+INPUT_TEXT_ERROR = "Invalid input, try again"
+ID_ERROR = "This ID does not exist"
